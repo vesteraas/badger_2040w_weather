ca958330510e0932238883a6399dd7666d80aed7
Werner Kvalem Vesterås <wvesteraas@gmail.com>
2023-09-15 11:04:38 +0200

    Refactoring.

--- display.py
+++ display.py
@@ -1,3 +1,6 @@
+from math import cos, sin
+
+
 def segments(number):
     if number == '0':
         return 'ABCDEF'
@@ -52,3 +55,144 @@ def draw_char(display, char, size, xo, yo):
                         ry = yo + p[1] * size + y
 
                         display.pixel(rx, ry)
+
+
+def draw_comma(display, size, xo, yo):
+    for y in range(0, size):
+        for x in range(0, size):
+            display.pixel(x + xo, y + yo)
+
+
+def draw_celsius(display, xo, yo):
+    display.image(
+        bytearray(
+            (
+                0b10000001,
+                0b00111100,
+                0b11111100,
+                0b11111100,
+                0b11111100,
+                0b11111100,
+                0b00111100,
+                0b10000001,
+            )
+        ),
+        8,
+        8,
+        xo,
+        yo,
+    )
+
+
+def draw_percent(display, xo, yo):
+    display.image(
+        bytearray(
+            (
+                0b00111000,
+                0b10011000,
+                0b11001000,
+                0b11100111,
+                0b00010011,
+                0b00011001,
+                0b00011100,
+                0b11111111,
+            )
+        ),
+        8,
+        8,
+        xo,
+        yo,
+    )
+
+
+def draw_temp(display, temp, yo):
+    display.set_font('bitmap8')
+    display.text('Temp', 0, yo)
+
+    is_negative = temp < 0
+
+    temp_str = '{:.1f}'.format(abs(temp))
+
+    whole = '' + temp_str[0:temp_str.index('.')]
+    frac = '' + temp_str[temp_str.index('.') + 1:]
+
+    if len(whole) == 1:
+        if is_negative:
+            draw_char(display, '-', 3, 24, yo + 18)
+
+        draw_char(display, whole[0], 3, 48, yo + 18)
+    else:
+        if is_negative:
+            draw_char(display, '-', 3, 0, yo + 18)
+
+        draw_char(display, whole[0], 3, 24, yo + 18)
+        draw_char(display, whole[1], 3, 48, yo + 18)
+
+    draw_comma(display, 3, 70, yo + 54)
+    draw_celsius(display, 80, yo + 18)
+    draw_char(display, frac, 2, 76, yo + 31)
+
+
+def draw_humid(display, humid, yo):
+    display.set_font('bitmap8')
+    display.text('Luftfukt', 0, yo)
+
+    humid_str = '{:.1f}'.format(humid)
+
+    whole = '' + humid_str[0:humid_str.index('.')]
+    frac = '' + humid_str[humid_str.index('.') + 1:]
+
+    if len(whole) == 1:
+        draw_char(display, humid_str[0], 3, 48, yo + 18)
+    else:
+        draw_char(display, humid_str[0], 3, 24, yo + 18)
+        draw_char(display, humid_str[1], 3, 48, yo + 18)
+
+    draw_comma(display, 3, 70, yo + 54)
+    draw_percent(display, 80, yo + 18)
+    draw_char(display, frac, 2, 76, yo + 31)
+
+
+def draw_arrow(display, angle, xo, yo):
+    angle = angle / 57.29
+
+    result = []
+
+    for vertex in [(-5, -25), (0, -20), (5, -25), (2, 15), (8, 12), (0, 25), (-8, 12), (-2, 15)]:
+        x = vertex[0]
+        y = vertex[1]
+
+        result.append(
+            (round(x * cos(angle) - y * sin(angle)) + xo, round(x * sin(angle) + y * cos(angle)) + yo))
+
+    display.polygon(result)
+
+
+def draw_compass(display, angle, xo, yo):
+    display.set_pen(0)
+    display.circle(xo, yo, 30)
+    display.set_pen(15)
+    display.circle(xo, yo, 28)
+    display.set_pen(0)
+    display.set_font('bitmap8')
+    display.text('N', xo - 4, yo - 46, 0)
+    display.text('NV', xo - 30, yo - 30, 0, 0.5)
+    display.text('NØ', xo + 24, yo - 30, 0, 0.5)
+    display.text('S', xo - 4, yo + 33, 0)
+    display.text('SV', xo - 30, yo + 25, 0, 0.5)
+    display.text('SØ', xo + 24, yo + 25, 0, 0.5)
+    display.text('V', xo - 40, yo - 7, 0)
+    display.text('Ø', xo + 33, yo - 7, 0)
+    draw_arrow(display, angle, xo, yo)
+
+
+def draw_wind_info(display, average, gust, xo, yo):
+    display.set_font('bitmap8')
+    display.text('Gj.snitt', xo, yo)
+    display.text(f'{average} m/s', 195, yo + 22)
+    display.text('I kastene', xo, yo + 54)
+    display.text(f'{gust} m/s', xo, yo + 76)
+
+
+def draw_precipication(display, rate, xo, yo):
+    display.text(f'Nedbør {rate} mm/t', xo, yo)
--- main.py
+++ main.py
@@ -3,154 +3,12 @@ import badger_os
 import urequests
 import ntptime
 
+from display import draw_temp, draw_humid, draw_compass, draw_wind_info, draw_precipication
+
 POSITION = (59.9138, 10.7387)
 UPDATES_PER_HOUR = 4
 
 USER_AGENT = 'Pimoroni Badger 2040W'
-
-from math import cos, sin
-from display import draw_char
-
-
-def draw_comma(display, size, xo, yo):
-    for y in range(0, size):
-        for x in range(0, size):
-            display.pixel(x + xo, y + yo)
-
-
-def draw_celsius(display, xo, yo):
-    display.image(
-        bytearray(
-            (
-                0b10000001,
-                0b00111100,
-                0b11111100,
-                0b11111100,
-                0b11111100,
-                0b11111100,
-                0b00111100,
-                0b10000001,
-            )
-        ),
-        8,
-        8,
-        xo,
-        yo,
-    )
-
-
-def draw_percent(display, xo, yo):
-    display.image(
-        bytearray(
-            (
-                0b00111000,
-                0b10011000,
-                0b11001000,
-                0b11100111,
-                0b00010011,
-                0b00011001,
-                0b00011100,
-                0b11111111,
-            )
-        ),
-        8,
-        8,
-        xo,
-        yo,
-    )
-
-
-def draw_temp(display, temp, yo):
-    display.set_font('bitmap8')
-    display.text('Temp', 0, yo)
-
-    is_negative = temp < 0
-
-    temp_str = '{:.1f}'.format(abs(temp))
-
-    whole = '' + temp_str[0:temp_str.index('.')]
-    frac = '' + temp_str[temp_str.index('.') + 1:]
-
-    if len(whole) == 1:
-        if is_negative:
-            draw_char(display, '-', 3, 24, yo + 18)
-
-        draw_char(display, whole[0], 3, 48, yo + 18)
-    else:
-        if is_negative:
-            draw_char(display, '-', 3, 0, yo + 18)
-
-        draw_char(display, whole[0], 3, 24, yo + 18)
-        draw_char(display, whole[1], 3, 48, yo + 18)
-
-    draw_comma(display, 3, 70, yo + 54)
-    draw_celsius(display, 80, yo + 18)
-    draw_char(display, frac, 2, 76, yo + 31)
-
-
-def draw_humid(display, humid, yo):
-    display.set_font('bitmap8')
-    display.text('Luftfukt', 0, yo)
-
-    humid_str = '{:.1f}'.format(humid)
-
-    whole = '' + humid_str[0:humid_str.index('.')]
-    frac = '' + humid_str[humid_str.index('.') + 1:]
-
-    if len(whole) == 1:
-        draw_char(display, humid_str[0], 3, 48, yo + 18)
-    else:
-        draw_char(display, humid_str[0], 3, 24, yo + 18)
-        draw_char(display, humid_str[1], 3, 48, yo + 18)
-
-    draw_comma(display, 3, 70, yo + 54)
-    draw_percent(display, 80, yo + 18)
-    draw_char(display, frac, 2, 76, yo + 31)
-
-
-def draw_arrow(display, angle, xo, yo):
-    angle = angle / 57.29
-
-    result = []
-
-    for vertex in [(-5, -25), (0, -20), (5, -25), (2, 15), (8, 12), (0, 25), (-8, 12), (-2, 15)]:
-        x = vertex[0]
-        y = vertex[1]
-
-        result.append(
-            (round(x * cos(angle) - y * sin(angle)) + xo, round(x * sin(angle) + y * cos(angle)) + yo))
-
-    display.polygon(result)
-
-
-def draw_compass(display, angle, xo, yo):
-    display.set_pen(0)
-    display.circle(xo, yo, 30)
-    display.set_pen(15)
-    display.circle(xo, yo, 28)
-    display.set_pen(0)
-    display.set_font('bitmap8')
-    display.text('N', xo - 4, yo - 46, 0)
-    display.text('NV', xo - 30, yo - 30, 0, 0.5)
-    display.text('NØ', xo + 24, yo - 30, 0, 0.5)
-    display.text('S', xo - 4, yo + 33, 0)
-    display.text('SV', xo - 30, yo + 25, 0, 0.5)
-    display.text('SØ', xo + 24, yo + 25, 0, 0.5)
-    display.text('V', xo - 40, yo - 7, 0)
-    display.text('Ø', xo + 33, yo - 7, 0)
-    draw_arrow(display, angle, xo, yo)
-
-
-def draw_wind_info(display, average, gust, xo, yo):
-    display.set_font('bitmap8')
-    display.text('Gj.snitt', xo, yo)
-    display.text(f'{average} m/s', 195, yo + 22)
-    display.text('I kastene', xo, yo + 54)
-    display.text(f'{gust} m/s', xo, yo + 76)
-
-
-def draw_precipication(display, rate, xo, yo):
-    display.text(f'Nedbør {rate} mm/t', xo, yo)
 
 
 def nowcast(position):
@@ -207,18 +65,18 @@ def connect(display):
 def set_time():
     try:
         ntptime.settime()
-    except Exeption as ex:
+    except Exception as ex:
         raise Exception('Failed to set time', ex)
 
 
 def run():
+    state = {
+        'cycles': 0,
+        'errors': 0
+    }
+
     try:
         badger = init()
-
-        state = {
-            'cycles': 0,
-            'errors': 0
-        }
 
         badger_os.state_load('weather', state)
 
@@ -256,8 +114,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-
-
-
-
